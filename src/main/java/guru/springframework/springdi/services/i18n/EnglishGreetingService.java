81dde49fc6137352feb94bc1c0d1d63b2f4ae5b8
NandorRefi <rnandor012@gmail.com>
2024-10-17 01:21:55 +0200

    Default profiles

--- src/main/java/guru/springframework/springdi/services/i18n/EnglishGreetingService.java
+++ src/main/java/guru/springframework/springdi/services/i18n/EnglishGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.springdi.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN") // context will only load this bean when it is set to EN profile
+@Profile({"EN", "default"}) // context will only load this bean when it is set to EN profile, we also set this as the default profile
 @Service("i18nService")
 public class EnglishGreetingService implements GreetingService {
     @Override
--- src/test/java/guru/springframework/springdi/controller/ConstructorInjectedControllerTest.java
+++ src/test/java/guru/springframework/springdi/controller/ConstructorInjectedControllerTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// notice that in the previous commit the other tests are failing because no profile is defined for them so they cannot create beans from the i18n services
+// so we need to specify a profile as default
 @SpringBootTest // loads spring context
 class ConstructorInjectedControllerTest {
 
